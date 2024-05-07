5fc929e3b44527f25ebce2a1c932269366a9587b
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2024-05-07 14:40:21 -0300

    new commit

--- js/e_noticias.js
+++ js/e_noticias.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){"use strict";var area_noticias=$(".e_area_noticias");if(area_noticias.find(".e_noticia").length>=2){var parametros_slide_noticias={elemento:"#e_slide_noticias",setas:!1,minwidth:280,rotacionar:5}
-var noticias_capa=function(){var noticias;if(window.innerWidth<=800){parametros_slide_noticias.slider=!0;sw_slider(parametros_slide_noticias)}else{parametros_slide_noticias.slider=!1;sw_slider(parametros_slide_noticias)}}
-sw_funcoes.push(noticias_capa)}})
+$(document).ready(function () {
+    "use strict"; var area_noticias = $(".e_area_noticias"); if (area_noticias.find(".e_noticia").length >= 2) {
+        var parametros_slide_noticias = { elemento: "#e_slide_noticias", setas: !1, minwidth: 280, rotacionar: 5 }
+        var noticias_capa = function () { var noticias; if (window.innerWidth <= 800) { parametros_slide_noticias.slider = !0; sw_slider(parametros_slide_noticias) } else { parametros_slide_noticias.slider = !1; sw_slider(parametros_slide_noticias) } }
+        sw_funcoes.push(noticias_capa)
+    }
+})
